ad7884770e6750580a70f6913be06b4f80d8c617
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-15 19:15:53 -0400

    feat: :sparkles: add and styles subscribeButton component

---
+++ src/components/SubscribeButton/subscribeButton.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+const SubscribeButton = () => {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe Now
+    </button>
+  );
+};
+
+export default SubscribeButton;
