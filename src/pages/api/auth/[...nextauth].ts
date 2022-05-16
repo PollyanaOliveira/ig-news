78e960e929dd46165797fff8bd85efcc3fff4718
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-16 13:23:43 -0400

    feat: :card_file_box: save user on database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth';
+import { query as q } from 'faunadb';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -12,5 +15,16 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      try {
+        const { email } = user;
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  }
 });
