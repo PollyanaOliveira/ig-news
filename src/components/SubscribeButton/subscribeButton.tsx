4d685cfcab629f93d80ce12b6441f1de53f1ec33
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-05-18 21:46:22 -0400

    feat: :alien: create checkout session checkout on stripe

--- src/components/SubscribeButton/subscribeButton.tsx
+++ src/components/SubscribeButton/subscribeButton.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +6,21 @@ interface SubscribeButtonProps {
 }
 
 const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession();
+
+  const handleSubscribe = () => {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+  };
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      onClick={handleSubscribe}
+      className={styles.subscribeButton}
+    >
       Subscribe Now
     </button>
   );
